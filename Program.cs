4113b1b15a23adfb7df5bbd933c98331bc193394
pleiterson <pleiterson@gmail.com>
2021-01-21 17:52:50 -0300

    create project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cadastro_series_dio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
